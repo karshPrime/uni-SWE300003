0dbca84851d9990196c5cc0227e3a65c09f58b11
4by3 <77736031+4by3@users.noreply.github.com>
2024-05-22 02:54:47 +1000

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
+#include "Payment.h"
+#include "InPersonCard.h"
+#include "Cash.h"
+#include "OnlinePayment.h"
+#include "User.h"
+#include "CreateUserFactory.h"
+#include "Kitchen.h"
+#include "Customer.h"
 
-int main()
-{
+int main() {
+    InPersonCard card;
+    Cash cash;
+    OnlinePayment online;
+
+    Payment payment(&card);
+    payment.make_Payment(100.0);
+
+    payment.setPaymentMethod(&cash);
+    payment.make_Payment(200.0);
+
+    payment.setPaymentMethod(&online);
+    payment.make_Payment(300.0);
+
+    User* employee = CreateUserFactory::createUser(EMPLOYEE);
+    User* customer = CreateUserFactory::createUser(CUSTOMER);
+    User* kitchen = CreateUserFactory::createUser(KITCHEN);
+
+    if (employee) {
+        employee->displayRole();
+    }
+
+    if (customer) {
+        customer->displayRole();
+    }
+
+    if (kitchen) {
+        kitchen->displayRole();
+    }
+
     return 0;
 }
-
